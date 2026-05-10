f58c0b2b14a231d133299465e8625bd97f3741ed
TenaarFeiri <tenaarfeiri@gmail.com>
2026-05-10 19:44:13 +0200

    Make login logic so that we can get critical data when we're sure we're ready.

--- YapperTongues.lua
+++ YapperTongues.lua
@@ -11,10 +11,21 @@ end
 
 -- It should be here anyway because Yapper is a required dep.
 -- If we get here, then we're all good. Initialise the addon.
-
-local init = Tongues.Core:Initialise()
-if not init then
-    -- If init failed, complain and return.
-    error("YapperTongues failed to initialise for some reason.")
-    return -- Not strictly necessary but doing it anyway.
+local loginListener = nil
+local function DoInit(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        local init = Tongues.Core:Initialise()
+        if not init then
+            -- If init failed, complain and return.
+            error("YapperTongues failed to initialise for some reason.")
+            return -- Not strictly necessary but doing it anyway.
+        end
+        -- We initialised. Unregister and kill the frame.
+        loginListener:UnregisterAllEvents()
+        loginListener = nil
+    end
 end
+
+loginListener = CreateFrame("Frame", "YapTongue")
+loginListener:RegisterEvent("PLAYER_LOGIN")
+loginListener:SetScript("OnEvent", DoInit)
